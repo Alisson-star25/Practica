fe344225320c887a30f1a8ead3a0050b9d7e19c8
Alisson-star25 <https://github.com/Alisson-star25/Prueba-II.git>
2026-06-08 17:16:16 -0600

    Se Modifico el archivo

--- Proyecto/Proyecto/Form1.Designer.cs
+++ Proyecto/Proyecto/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Open Sans", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(44, 120);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(702, 163);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "PROYECTO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl1;
     }
 }
 
